45cfc872ac691c0f07ae76573b6de7885ee8495a
Carlos Guerra Martins <132399324+carlosguerram@users.noreply.github.com>
2024-06-25 15:51:21 -0300

    finalizando exercício

--- aula12/ex015/script.js
+++ aula12/ex015/script.js
@@ -1,6 +1,51 @@
-function verificar() {
+function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-     
+    var res = document.querySelector('div#res')
+    if (fano.value.lenght == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO]Verifique os dados e tente novamente.')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'homemcriança.png')
+            } else if (idade <21) {
+                //jovem
+                img.setAttribute('src', 'homemjovem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homemadulto.png')
+            } else{
+                //idoso
+                img.setAttribute('src', 'homemvelho.png')
+            }
+        }  else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'mulhercriança.png')
+            } else if (idade <21){
+                //jovem
+                img.setAttribute('src', 'mulherjovem.png')
+            } else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'mulheradulta.png')
+            } else{
+                //idoso
+                img.setAttribute('src', 'mulhervelha.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+
+    }
+
 }
